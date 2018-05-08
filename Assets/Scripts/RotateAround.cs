10010a27515aa9975f5832747a5da687e2f6826c
enki390 <enki390@gmail.com>
2018-05-09 02:49:27 +0900

    * add rorate tutorial script

---
+++ Assets/Scripts/RotateAround.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+    public float Distance = 5.0f;
+    public float AnglePerSec = 90.0f;
+
+	// Update is called once per frame
+	void Update () {
+        Transform parent = transform.parent;
+        Vector3 direction = transform.localPosition.normalized;
+        float angle = AnglePerSec * Time.deltaTime;
+        direction = (Quaternion.AngleAxis(angle, Vector3.up) * direction) * Distance;
+        transform.localPosition = direction;
+	}
+}
---
+++ Assets/Scripts/RotateAroundTarget.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundTarget : MonoBehaviour {
+    public Transform Target;
+    public float Distance = 5.0f;
+    public float AnglePerSec = 90.0f;
+    float currentAngle = 0.0f;
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 direction = (transform.position - Target.position).normalized;
+        float angle = AnglePerSec * Time.deltaTime;
+        currentAngle = (currentAngle + angle) % 360.0f;
+        if (currentAngle < 0)
+            currentAngle += 360.0f;
+
+        direction = Quaternion.AngleAxis(currentAngle, Target.up) * Target.right;
+        transform.position = Target.position + (direction * Distance);
+	}
+}
