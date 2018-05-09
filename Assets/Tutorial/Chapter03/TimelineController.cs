00aa5aa4fa3be8fb0e2d67992806fac14b9f24fc
enki390 <enki390@gmail.com>
2018-05-10 02:34:41 +0900

    * Updated chapter1 * Added completed Chapter02

---
+++ Assets/Tutorial/Chapter03/TimelineController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class TimelineController : MonoBehaviour {
+
+	public List<PlayableDirector> Directors;
+	public List<TimelineAsset> Timelines;
+
+	public void Play()
+	{
+		foreach (PlayableDirector pd in Directors) {
+			pd.Play ();
+		}
+	}
+
+	public void PlayFromTimelines(int index)
+	{
+		TimelineAsset selectedTimeline;
+		if (Timelines.Count != index) {
+			selectedTimeline = Timelines [Timelines.Count - 1];
+		} else {
+			selectedTimeline = Timelines [index];
+		}
+
+		Directors [0].Play (selectedTimeline);			
+	}
+}
